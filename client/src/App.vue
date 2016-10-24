fe4894c04a0a451ca269657a3e0935da6a6d039b
fouding <fouding.zheng@gmail.com>
2016-10-24 11:10:57 +0800

    use a wrapper App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,8 +7,8 @@
           Vue Time Tracker
         </a>
         <ul class="nav navbar-nav">
-          <li><a v-link="'/home'">Home</a></li>
-          <li><a v-link="'/time-entries'">Time Entries</a></li>
+          <li><router-link to="/home">Home</router-link></li>
+          <li><router-link to="/time-entries">Time Entries</router-link></li>
         </ul>
       </div>
     </nav>
@@ -17,7 +17,7 @@
         <sidebar :time="totalTime"></sidebar>
       </div>
       <div class="col-sm-9">
-        <router-view></router-view>
+        <router-view @deleteTime="deleteTime" @timeUpdate="timeUpdate"></router-view>
       </div>
     </div>
   </div>
@@ -37,7 +37,7 @@
         totalTime: 1.5
       }
     },
-    events: {
+        methods: {
       // Increment the totalTime value based on the new
       // time entry that is dispatched up
       timeUpdate (timeEntry) {
--- client/src/main.js
+++ client/src/main.js
@@ -31,57 +31,8 @@ var bus = new Vue()
 
 const app = new Vue({
     router,
-    template: `
-        <div id="wrapper">
-        <nav class="navbar navbar-default">
-            <div class="container">
-            <a class="navbar-brand" href="#">
-                <i class="glyphicon glyphicon-time"></i>
-                Vue Time Tracker
-            </a>
-            <ul class="nav navbar-nav">
-                <li><router-link to="/home">Home</router-link></li>
-                <li><router-link to="/time-entries">Time Entries</router-link></li>
-            </ul>
-            </div>
-        </nav>
-        <div class="container">
-            <div class="col-sm-3">
-            <sidebar :time="totalTime"></sidebar>
-            </div>
-            <div class="col-sm-9">
-            <router-view @deleteTime="deleteTime" @timeUpdate="timeUpdate"></router-view>
-            </div>
-        </div>
-        </div>
-    `,
     components: {
-        'sidebar': Sidebar
-    },
-    data() {
-        return {
-            // Start with the same value as our
-            // first time entry. Hard-coded for now
-            // because we'll use a different approach
-            // in the next article anyway
-            totalTime: 1.5
-      }
-    },
-    methods: {
-        evt: function() {
-            console.log('router-view event');
-        },
-        // Increment the totalTime value based on the new
-        // time entry that is dispatched up
-        timeUpdate(timeEntry) {
-            console.log('main.js: timeEntry: ' + timeEntry)
-            this.totalTime += parseFloat(timeEntry.totalTime)
-        },
-        // Decrement totalTime when a time entry is deleted
-        deleteTime(timeEntry) {
-            console.log('timeEntry: ' + timeEntry)
-            this.totalTime -= parseFloat(timeEntry.totalTime)
+        App
     }
-  }
 
 }).$mount('#app')
